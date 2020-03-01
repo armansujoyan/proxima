06b880e433b2b55136ff0e65d825dee8c98e57cf
Arman Sujoyan <asujoyan@gmail.com>
2020-03-01 13:58:37 +0400

    feat(model_loader) Wrap all Gl calls
    Wrap all OpenGL calls into debuggable function

--- src/renderer/Mesh.cpp
+++ src/renderer/Mesh.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Mesh.h"
+#include "../utils/Debug.h"
 
 Mesh::Mesh(const std::string& path) {
     InitializeMesh(IndexedObject(path));
@@ -11,57 +12,57 @@ Mesh::Mesh(const std::string& path) {
 void Mesh::InitializeMesh(const IndexedObject& model) {
     m_IndexCount = model.m_indices.size();
 
-    glGenVertexArrays(1, &m_VertexArrayID);
-    glBindVertexArray(m_VertexArrayID);
+    GLCall(glGenVertexArrays(1, &m_VertexArrayID));
+    GLCall(glBindVertexArray(m_VertexArrayID));
 
-    glGenBuffers(m_BufferCount, m_VertexBuffers);
+    GLCall(glGenBuffers(m_BufferCount, m_VertexBuffers));
 
-    glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[POSITION_BUFFER]);
-    glBufferData(
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[POSITION_BUFFER]));
+    GLCall(glBufferData(
             GL_ARRAY_BUFFER,
             sizeof(model.m_indexed_positions[0]) * model.m_indexed_positions.size(),
             &model.m_indexed_positions[0],
-            GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+            GL_STATIC_DRAW));
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 0, 0));
 
-    glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[TEXTURE_BUFFER]);
-    glBufferData(
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[TEXTURE_BUFFER]));
+    GLCall(glBufferData(
             GL_ARRAY_BUFFER,
             sizeof(model.m_indexed_textures[0]) * model.m_indexed_textures.size(),
             &model.m_indexed_textures[0],
-            GL_STATIC_DRAW);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+            GL_STATIC_DRAW));
+    GLCall(glEnableVertexAttribArray(1));
+    GLCall(glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0));
 
-    glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[NORMAL_BUFFER]);
-    glBufferData(
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffers[NORMAL_BUFFER]));
+    GLCall(glBufferData(
             GL_ARRAY_BUFFER,
             sizeof(model.m_indexed_normals[0]) * model.m_indexed_normals.size(),
             &model.m_indexed_normals[0],
-            GL_STATIC_DRAW);
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+            GL_STATIC_DRAW));
+    GLCall(glEnableVertexAttribArray(2));
+    GLCall(glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0));
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_VertexBuffers[INDEX_BUFFER]);
-    glBufferData(
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_VertexBuffers[INDEX_BUFFER]));
+    GLCall(glBufferData(
             GL_ELEMENT_ARRAY_BUFFER,
             sizeof(model.m_indices[0]) * model.m_indices.size(),
             &model.m_indices[0],
-            GL_STATIC_DRAW);
+            GL_STATIC_DRAW));
 
-    glBindVertexArray(0);
+    GLCall(glBindVertexArray(0));
 }
 
 void Mesh::Draw() {
-    glBindVertexArray(m_VertexArrayID);
+    GLCall(glBindVertexArray(m_VertexArrayID));
 
-    glDrawElements(GL_TRIANGLES, m_IndexCount, GL_UNSIGNED_INT, 0);
+    GLCall(glDrawElements(GL_TRIANGLES, m_IndexCount, GL_UNSIGNED_INT, 0));
 
-    glBindVertexArray(0);
+    GLCall(glBindVertexArray(0));
 }
 
 Mesh::~Mesh() {
-    glDeleteBuffers(m_BufferCount, m_VertexBuffers);
-    glDeleteVertexArrays(1, &m_VertexArrayID);
+    GLCall(glDeleteBuffers(m_BufferCount, m_VertexBuffers));
+    GLCall(glDeleteVertexArrays(1, &m_VertexArrayID));
 }
