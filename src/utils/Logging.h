18efe040e2be3d3f104cbf1c353f94fc87085083
Arman Sujoyan <asujoyan@gmail.com>
2020-04-04 18:13:29 +0400

    feat(utils) Add logging and string utils
    Logging utils:
        printUnhandledFileMessage()
    String utils:
        ParseVec3();
        ParseVec2();
        getRelativePath();

---
+++ src/utils/Logging.h
@@ -0,0 +1,13 @@
+//
+// Created by Arman Sujoyan on 4/4/20.
+//
+
+#ifndef PROXIMA_LOGGING_H
+#define PROXIMA_LOGGING_H
+
+
+static void printUnhandledFileMessage() {
+    std::cout << "Error: Cannot open the file for loading the model" << std::endl;
+}
+
+#endif //PROXIMA_LOGGING_H
--- src/utils/StringUtils.h
+++ src/utils/StringUtils.h
@@ -10,7 +10,7 @@
 #include <algorithm>
 
 template <class Container>
-void split(const std::string& str, Container& cont, char delim = ' ') {
+static void split(const std::string& str, Container& cont, char delim = ' ') {
     std::size_t current, previous = 0;
     current = str.find(delim);
     while(current != std::string::npos) {
@@ -19,6 +19,39 @@ void split(const std::string& str, Container& cont, char delim = ' ') {
         current = str.find(delim, previous);
     }
     cont.push_back(str.substr(previous, current - previous));
+}
+
+static glm::vec3 ParseVec3(const std::string &line) {
+    std::stringstream ss(line);
+    float parsedValues[3];
+
+    for (float &parsedValue : parsedValues) {
+        ss >> parsedValue;
+    }
+
+    return glm::vec3(
+            parsedValues[0],
+            parsedValues[1],
+            parsedValues[2]);
+}
+
+static glm::vec2 ParseVec2(const std::string &line) {
+    std::stringstream ss(line);
+    float parsedValues[3];
+
+    for (int i = 0; i < 2; ++i) {
+        ss >> parsedValues[i];
+    }
+
+    return glm::vec2(
+            parsedValues[0],
+            parsedValues[1]);
+}
+
+static std::string getRelativePath(const std::string &rootPath, const std::string &fileName) {
+    size_t lastSlashPosition = rootPath.find_last_of('/');
+    std::string relativePath = rootPath.substr(0, lastSlashPosition + 1) + fileName;
+    return relativePath;
 }
 
 #endif //PROXIMA_STRINGUTILS_H
