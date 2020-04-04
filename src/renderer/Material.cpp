7056459f06fc7a10c0a2e891fcc5cfe2a30213b8
Arman Sujoyan <asujoyan@gmail.com>
2020-04-04 18:23:20 +0400

    feat(material) Add material meta and vectorize
    Att MaterialMeta struct for material metadata
    and vectorize lighting coefficients.

--- src/renderer/Material.cpp
+++ src/renderer/Material.cpp
@@ -6,6 +6,16 @@
 
 Material::Material() = default;
 
+Material::Material(MaterialMeta &meta) : m_key_diffuse(meta.Kd), m_key_ambient(meta.Ka), m_key_specular(meta.Ks) {
+    if (!meta.map_Kd.empty()) {
+        m_diffuse_texture = Texture(meta.map_Kd.c_str(), true, 0, GL_RGB);
+    }
+
+    if (!meta.map_Ks.empty()) {
+        m_specular_texture = Texture(meta.map_Kd.c_str(), true, 1, GL_RGB);
+    }
+}
+
 void Material::addDiffuseMap(Texture &diffuse) {
     m_diffuse_texture = diffuse;
 }
--- src/renderer/Material.h
+++ src/renderer/Material.h
@@ -10,18 +10,28 @@
 #include "Texture.h"
 #include "Shader.h"
 
+struct MaterialMeta {
+    std::string name;
+    std::string map_Kd;
+    std::string map_Ks;
+    glm::vec3 Kd;
+    glm::vec3 Ka;
+    glm::vec3 Ks;
+};
+
 class Material {
 public:
     Material();
-    Material(float key_diffuse, float key_ambient, float key_specular):
+    Material(glm::vec3 key_diffuse, glm::vec3 key_ambient, glm::vec3 key_specular):
         m_key_diffuse(key_diffuse), m_key_ambient(key_ambient), m_key_specular(key_specular) {};
+    explicit Material(MaterialMeta &meta);
     void addDiffuseMap(Texture& diffuse);
     void addSpecularMap(Texture& specular);
     void addShader(Shader& shader);
 private:
-    float m_key_diffuse{};
-    float m_key_ambient{};
-    float m_key_specular{};
+    glm::vec3 m_key_diffuse{};
+    glm::vec3 m_key_ambient{};
+    glm::vec3 m_key_specular{};
     Texture m_diffuse_texture;
     Texture m_specular_texture;
     Shader m_shader;
