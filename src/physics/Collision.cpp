5847d88762349c3815eccc2170960ae4a07b7074
Arman Sujoyan <asujoyan@gmail.com>
2020-04-16 00:50:44 +0400

    fix(collision) Minor fixes

--- src/physics/Collision.cpp
+++ src/physics/Collision.cpp
@@ -5,54 +5,6 @@
 #include <glm/glm.hpp>
 #include "Collision.h"
 
-bool Collision::isPointInsideTriangle(const glm::vec3 &point, const glm::vec3 &vertex1, const glm::vec3 &vertex2,
-                                      const glm::vec3 &vertex3, const glm::vec3 &triangleNormal) {
-    glm::vec3 edge, edgeNormal, pointToEdge;
-
-    edge = vertex2 - vertex1;
-    pointToEdge = vertex1 - point;
-    edgeNormal = glm::cross(edge, triangleNormal);
-
-    if (glm::dot(pointToEdge, triangleNormal) < 0.0f) return false;
-
-    edge = vertex3 - vertex2;
-    pointToEdge = vertex2 - point;
-    edgeNormal = glm::cross(edge, triangleNormal);
-
-    if (glm::dot(pointToEdge, triangleNormal) < 0.0f) return false;
-
-    edge = vertex1 - vertex3;
-    pointToEdge = vertex3 - point;
-    edgeNormal = glm::cross(edge, triangleNormal);
-
-    return !(glm::dot(pointToEdge, triangleNormal) < 0.0f);
-
-}
-
-bool Collision::sphereIntersectPlane(const glm::vec3 &center, float radius, const glm::vec3 &velocity,
-                                     const glm::vec3 &planeNormal, const glm::vec3 &planePoint, float &tMax) {
-    float numerator = glm::dot(center - planePoint, planeNormal);
-    float denominator = glm::dot(velocity, planeNormal);
-
-    if (numerator < 0.0f || denominator > -0.000001f) {
-
-        if (denominator > -1e-5f) return false;
-
-        if (numerator < -radius) return false;
-
-        tMax = numerator;
-
-        return true;
-    }
-
-    float t = -(numerator/denominator);
-
-    if (t < 0.0f || t > tMax) return false;
-
-    tMax = t;
-
-    return true;
-}
 
 bool Collision::sphereIntersectTriangle(const glm::vec3 &center, float radius, const glm::vec3 &velocity,
                                         const glm::vec3 &vertex1, const glm::vec3 &vertex2, const glm::vec3 &vertex3,
@@ -84,6 +36,55 @@ bool Collision::sphereIntersectTriangle(const glm::vec3 &center, float radius, c
     return edgeCollided;
 }
 
+bool Collision::isPointInsideTriangle(const glm::vec3 &point, const glm::vec3 &vertex1, const glm::vec3 &vertex2,
+                                      const glm::vec3 &vertex3, const glm::vec3 &triangleNormal) {
+    glm::vec3 edge, edgeNormal, pointToEdge;
+
+    edge = vertex2 - vertex1;
+    pointToEdge = vertex1 - point;
+    edgeNormal = glm::cross(edge, triangleNormal);
+
+    if (glm::dot(pointToEdge, triangleNormal) < 0.0f) return false;
+
+    edge = vertex3 - vertex2;
+    pointToEdge = vertex2 - point;
+    edgeNormal = glm::cross(edge, triangleNormal);
+
+    if (glm::dot(pointToEdge, triangleNormal) < 0.0f) return false;
+
+    edge = vertex1 - vertex3;
+    pointToEdge = vertex3 - point;
+    edgeNormal = glm::cross(edge, triangleNormal);
+
+    return !(glm::dot(pointToEdge, triangleNormal) < 0.0f);
+
+}
+
+bool Collision::sphereIntersectPlane(const glm::vec3 &center, float radius, const glm::vec3 &velocity,
+                                     const glm::vec3 &planeNormal, const glm::vec3 &planePoint, float &tMax) {
+    float numerator = glm::dot(center - planePoint, planeNormal) - radius;
+    float denominator = glm::dot(velocity, planeNormal);
+
+    if (numerator < 0.0f || denominator > -0.000001f) {
+
+        if (denominator > -1e-5f) return false;
+
+        if (numerator < -radius) return false;
+
+        tMax = numerator;
+
+        return true;
+    }
+
+    float t = -(numerator/denominator);
+
+    if (t < 0.0f || t > tMax) return false;
+
+    tMax = t;
+
+    return true;
+}
+
 bool Collision::sphereIntersectLineSegment(const glm::vec3 &center, float radius, const glm::vec3 &velocity,
                                            const glm::vec3 &vertex1, const glm::vec3 &vertex2, float &tMax,
                                            glm::vec3 &collisionNormal) {
@@ -97,7 +98,7 @@ bool Collision::sphereIntersectLineSegment(const glm::vec3 &center, float radius
 
     if (edgeLength < 1e-5f) return false;
 
-    edge = edge / edgeLength;
+    edge /= edgeLength;
 
     glm::vec3 centerToVertexEdgeCross = glm::cross(centerToVertex, edge);
     glm::vec3 velocityEdgeCross = glm::cross(velocity, edge);
